04a56c4854f0ce67b888acd1291d9c19375c4e9c
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-11 11:27:23 -0300

    Instalação e uso da lib styled

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,11 +1,36 @@
+import styled from "styled-components";
 import Menu from "../Menu";
+
+const periodo = "diurno";
+
+/* Tagged Template para uso com styled components: `` */
+const StyledCabecalho = styled.header`
+  background-color: ${periodo === "diurno" ? "lightblue" : "darkblue"};
+  text-align: center;
+
+  h1 {
+    color: red;
+  }
+
+  p {
+    font-weight: bold;
+    color: blue;
+    font-size: 2rem;
+
+    &:hover {
+      color: red;
+      cursor: pointer;
+    }
+  }
+`;
 
 const Cabecalho = () => {
   return (
-    <header>
+    <StyledCabecalho>
       <h1>Interface React</h1>
       <Menu />
-    </header>
+      <p>Teste</p>
+    </StyledCabecalho>
   );
 };
 
