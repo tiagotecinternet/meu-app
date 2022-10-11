87d0e5bb36601fd1c3ae0337ec36f97f1b7cf258
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-11 08:43:14 -0300

    CSS externo e classe

--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,13 +1,11 @@
 import Artigo from "../Artigo";
+import "./Conteudo.css";
 
 const Conteudo = () => {
-  // Comentário JS ctrl ;
-  /* Comentário JS shift alt a */
   return (
     <main>
       <section>
-        {/* AQUI É COMENTÁRIO DENTRO DO JSX  */}
-        <h2>Conteúdo do site....</h2>
+        <h2 className="subtitulo">Conteúdo do site....</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ab
           inventore, sapiente impedit numquam maxime quia voluptates, et at modi
