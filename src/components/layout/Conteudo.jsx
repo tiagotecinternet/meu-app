aa7220b9b914dd0cfbcfc211a67cb0770c83d76f
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-13 11:33:49 -0300

    Mais exemplos useState

--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import Artigo from "../Artigo";
 
 const Conteudo = () => {
-  // Comentário JS ctrl ;
-  /* Comentário JS shift alt a */
+  const [corFundo, setCorFundo] = useState();
+  const [contador, setContador] = useState(0);
+
+  const fundoAmarelo = () => setCorFundo("#FFEE71");
+  const fundoAzul = () => setCorFundo("lightblue");
+  const fundoVermelho = () => setCorFundo("lightpink");
+
+  const atualizaContagem = () => setContador(contador + 1);
+
   return (
-    <main>
+    <main style={{ backgroundColor: corFundo }}>
+      <button onMouseOver={fundoAmarelo}>Amarelo</button>
+      <button onMouseOver={fundoAzul}>Azul</button>
+      <button onMouseOver={fundoVermelho}>Vermelho</button>
+
       <section>
-        {/* AQUI É COMENTÁRIO DENTRO DO JSX  */}
-        <h2>Conteúdo do site....</h2>
+        <h2 onClick={atualizaContagem}>Contador: {contador} </h2>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ab
           inventore, sapiente impedit numquam maxime quia voluptates, et at modi
