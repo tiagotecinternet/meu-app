01f04b652f38abb55c22c86f0084e05a000fe55f
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-13 11:06:55 -0300

    Exemplo 1 de useState

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Menu from "../Menu";
 
 const Cabecalho = () => {
@@ -5,12 +6,24 @@ const Cabecalho = () => {
     console.log("exemplo2");
   };
 
+  /* Exemplo 1 de gerenciamento de states
+  Aqui, definimos no useState um valor inicial (colocado entre os parênteses do useState), além de desestruturar o state em uma variável (titulo) e em uma
+  função (setTitulo) que será responsável por atualizar este state/título */
+  const [titulo, setTitulo] = useState("Exemplo de State");
+
+  const atualizaTitulo = () => {
+    setTitulo("Opa, state do titulo foi alterado!");
+  };
+
   return (
     <header>
       <button onClick={() => console.log("exemplo1")}>Exemplo 1</button>
       <button onClick={exemplo2}>Exemplo 2</button>
       <hr />
-      <h1>Interface React</h1>
+
+      {/* Aqui, aplicamos a variável de state chamada titulo */}
+      <h1 onClick={atualizaTitulo}> {titulo} </h1>
+
       <Menu />
     </header>
   );
