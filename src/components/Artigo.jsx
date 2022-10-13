c23eebbf2aae5486cf8559ac8b6bd51c23e7ecdf
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-13 09:19:01 -0300

    Exercício de props com array

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,6 +1,9 @@
 const Artigo = (props) => {
   return (
     <article>
+      <h3> {props.titulo} </h3>
+      <h4> {props.subtitulo} </h4>
+      <p>Curso: {props.curso}</p>
       <p> {props.children} </p>
     </article>
   );
--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,6 +1,11 @@
 import Artigo from "../Artigo";
 
 const Conteudo = () => {
+  /* 1) Crie um array com o nome de 3 cursos */
+  const cursos = ["JS Avançado", "React", "Node.js"];
+
+  /* 2) Dentro das chamadas dos artigos, passe como uma nova prop chamada curso
+  o nome de cada curso. O primeiro artigo deve mostrar o nome do primeiro curso, o segundo artigo o nome do segundo curso e o terceiro o nome do terceiro curso. */
   return (
     <main>
       <section>
@@ -12,9 +17,27 @@ const Conteudo = () => {
           commodi voluptate fugit.
         </p>
 
-        <Artigo>Este é o texto do primeiro artigo....</Artigo>
-        <Artigo>Este é o texto do segundo artigo....</Artigo>
-        <Artigo>Este é o texto do terceiro artigo....</Artigo>
+        <Artigo
+          curso={cursos[0]}
+          titulo="Artigo 1"
+          subtitulo="Subtítulo do artigo 1"
+        >
+          Este é o texto do primeiro artigo....
+        </Artigo>
+        <Artigo
+          curso={cursos[1]}
+          subtitulo="Subtítulo do artigo 2"
+          titulo="Artigo 2"
+        >
+          Este é o texto do segundo artigo....
+        </Artigo>
+        <Artigo
+          curso={cursos[2]}
+          titulo="Artigo 3"
+          subtitulo="Subtítulo do artigo 3"
+        >
+          Este é o texto do terceiro artigo....
+        </Artigo>
       </section>
     </main>
   );
