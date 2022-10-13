84fb599ba315939167afd4e412b854bf0c010b71
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-13 10:40:53 -0300

    Exemplos de eventos

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,8 +1,15 @@
 import Menu from "../Menu";
 
 const Cabecalho = () => {
+  const exemplo2 = () => {
+    console.log("exemplo2");
+  };
+
   return (
     <header>
+      <button onClick={() => console.log("exemplo1")}>Exemplo 1</button>
+      <button onClick={exemplo2}>Exemplo 2</button>
+      <hr />
       <h1>Interface React</h1>
       <Menu />
     </header>
