7cd60cb56c83294c271ba83d74254ffd54d350ce
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-14 08:44:15 -0300

    Remoção do Conteudo, criação do Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import Artigo from "../components/Artigo";
+
+const Home = () => {
+  return (
+    <>
+      <h2>Página HOME</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A veniam
+        voluptates enim rem beatae assumenda, nihil recusandae perspiciatis
+        voluptate hic repellendus eveniet impedit eum accusamus ex. Praesentium
+        possimus sed atque.
+      </p>
+      <Artigo />
+      <Artigo />
+      <Artigo />
+    </>
+  );
+};
+
+export default Home;
