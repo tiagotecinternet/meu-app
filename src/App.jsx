5ce546115b0a9c1606841724310580787558093e
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-14 09:13:41 -0300

    Adição das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import Cabecalho from "./components/layout/Cabecalho";
 import Rodape from "./components/layout/Rodape";
 import Home from "./pages/Home";
@@ -7,11 +9,23 @@ import Servicos from "./pages/Servicos";
 const App = () => {
   return (
     <>
-      <Cabecalho />
-      <Home />
-      <Produtos />
-      <Servicos />
-      <Rodape />
+      <BrowserRouter>
+        <Cabecalho />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/produtos">
+            <Produtos />
+          </Route>
+          <Route path="/servicos">
+            <Servicos />
+          </Route>
+        </Switch>
+
+        <Rodape />
+      </BrowserRouter>
     </>
   );
 };
